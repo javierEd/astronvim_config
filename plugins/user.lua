e3143ac4a66688e7418ea3d39c276a416303e2e4
Javier E. <jaedns@gmail.com>
2024-04-01 12:26:50 -0300

    Improvements on the statusbar

--- plugins/user.lua
+++ plugins/user.lua
@@ -23,12 +23,12 @@ return {
   --   dependencies = "rcarriga/nvim-notify",
   --   opts = {},
   -- },
-  {
-    "gaborvecsei/usage-tracker.nvim",
-    lazy = true,
-    event = { "BufEnter" },
-    opts = { keep_eventlog_days = 30, inactivity_threshold_in_min = 1 },
-  },
+  -- {
+  --   "gaborvecsei/usage-tracker.nvim",
+  --   lazy = true,
+  --   event = { "BufEnter" },
+  --   opts = { keep_eventlog_days = 30, inactivity_threshold_in_min = 1 },
+  -- },
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
@@ -42,56 +42,78 @@ return {
     "rebelot/heirline.nvim",
     opts = function(_, opts)
       local status = require "astronvim.utils.status"
+      local job = require "plenary.job"
 
       local trim = function(value) return string.gsub(value, "^%s*(.-)%s*$", "%1") end
 
-      local stats_provider = function(self) return self.prefix .. self.stats end
+      local wt_today = "0 secs"
+      local wt_cli_location = ""
 
-      local waka_time_today = "0 secs"
+      local wt_condition = function(self)
+        if self.job ~= nil then return true end
 
-      local waka_time_today_init = function(self)
-        self.prefix = "󰄉 WT "
-        self.stats = waka_time_today
+        wt_cli_location = trim(vim.fn.execute("WakaTimeCliLocation", "silent!"))
 
-        local waka_time_cli_location = trim(vim.fn.execute("WakaTimeCliLocation", "silent!"))
+        if wt_cli_location ~= "" then return true end
 
-        if waka_time_cli_location == "" then return end
+        return false
+      end
 
-        local job = require "plenary.job"
+      local wt_has_to_start_job = function(self) return self.current_time == nil or self.current_time + 60 <= os.time() end
 
-        job
-          :new({
-            command = waka_time_cli_location,
+      local wt_today_has_changed = function(self) return self.today ~= wt_today end
+
+      local wt_today_init = function(self)
+        if self.job == nil then
+          self.job = job:new {
+            command = wt_cli_location,
             args = { "--today" },
-            on_exit = function(j, _) waka_time_today = trim(j:result()[1]) end,
-          })
-          :start()
+            on_exit = function(j, _)
+              local today = trim(j:result()[1])
 
-        self.stats = waka_time_today
-      end
+              if today == "" then return end
 
-      local usage_tracker_init = function(self)
-        self.prefix = "󰄉 UT "
-        self.stats = "0 secs"
+              wt_today = trim(j:result()[1])
+            end,
+          }
 
-        local usage_tracker_daily = trim(vim.fn.execute("UsageTrackerShowDailyAggregation", "silent!"))
-        local minutes_str = string.match(usage_tracker_daily, "%d+.%d+$")
-
-        if minutes_str == nil then return end
-
-        local minutes = tonumber(minutes_str)
-
-        if minutes == nil then return end
-
-        if minutes >= 60 then
-          self.stats = string.format("%.2f", minutes / 60) .. " hours"
-        elseif minutes >= 1 then
-          self.stats = minutes_str .. " mins"
-        else
-          local seconds = (minutes * 60) / 100
-          self.stats = string.format("%.2f", seconds) .. " secs"
+          self.prefix = "󰄉 WT "
+          self.today = wt_today
         end
+
+        if wt_has_to_start_job(self) then self.job:start() end
+
+        if wt_today_has_changed(self) then self.today = wt_today end
+
+        self.current_time = os.time()
       end
+
+      local wt_provider = function(self) return self.prefix .. self.today end
+
+      local wt_update = function(self) return wt_today_has_changed(self) or wt_has_to_start_job(self) end
+
+      -- local usage_tracker_init = function(self)
+      --   self.prefix = "󰄉 UT "
+      --   self.stats = "0 secs"
+      --
+      --   local usage_tracker_daily = trim(vim.fn.execute("UsageTrackerShowDailyAggregation", "silent!"))
+      --   local minutes_str = string.match(usage_tracker_daily, "%d+.%d+$")
+      --
+      --   if minutes_str == nil then return end
+      --
+      --   local minutes = tonumber(minutes_str)
+      --
+      --   if minutes == nil then return end
+      --
+      --   if minutes >= 60 then
+      --     self.stats = string.format("%.2f", minutes / 60) .. " hours"
+      --   elseif minutes >= 1 then
+      --     self.stats = minutes_str .. " mins"
+      --   else
+      --     local seconds = (minutes * 60) / 100
+      --     self.stats = string.format("%.2f", seconds) .. " secs"
+      --   end
+      -- end
 
       opts.statusline = { -- statusline
         hl = { fg = "fg", bg = "bg" },
@@ -103,10 +125,15 @@ return {
         status.component.fill(),
         status.component.cmd_info(),
         status.component.fill(),
-        { init = waka_time_today_init, provider = stats_provider, update = "BufEnter" },
-        { provider = "  " },
-        { init = usage_tracker_init, provider = stats_provider, update = "BufEnter" },
-        { provider = " " },
+        {
+          condition = wt_condition,
+          init = wt_today_init,
+          provider = wt_provider,
+          update = wt_update,
+        },
+        -- { provider = "  " },
+        -- { init = usage_tracker_init, provider = stats_provider, update = "BufEnter" },
+        -- { provider = " " },
         status.component.lsp(),
         status.component.treesitter(),
         status.component.nav(),
